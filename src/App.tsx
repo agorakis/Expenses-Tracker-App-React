5abc8c94a1d6542be3573bcb31a4ad9d87711ac2
Antonis Gorakis <agorakis@gmail.com>
2023-04-20 00:26:17 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="p-4 bg-light text-center">
+        <h2 className="pb-4"> Manage your expenses clever... </h2>
+
+        <div className="row row-cols-1 row-cols-lg-2 justify-content-around gy-3">
+          <div className="col">One of two columns</div>
+          <div className="col">One of two columns</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
