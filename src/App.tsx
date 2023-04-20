f93fab67164efdc5acd17dffc57a6ce42c904431
Antonios Gkorakis <agkorakis@fxcess.com>
2023-04-20 16:42:20 +0300

    Finalize the Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { BsCalculator, BsFillCreditCardFill } from "react-icons/bs";
+import ExpenseList from "./ExpenseList";
+
 function App() {
   return (
     <>
       <div className="p-4 bg-light text-center">
-        <h2 className="pb-4"> Manage your expenses clever... </h2>
+        <h2 className="pb-4"> <BsCalculator color="green" size={30}/> Manage your expenses <BsFillCreditCardFill color="orange" size={30}/> efficiently </h2>
 
         <div className="row row-cols-1 row-cols-lg-2 justify-content-around gy-3">
           <div className="col">One of two columns</div>
-          <div className="col">One of two columns</div>
+          <div className="col"><ExpenseList/></div>
         </div>
       </div>
+      <div className="ps-2 bg-secondary text-warning"> Coded by <span className="fw-bold fst-italic">antGor...</span></div>
     </>
   );
 }
 
 export default App;
+
+
---
+++ src/ExpenseList.tsx
@@ -0,0 +1,5 @@
+const ExpenseList = () => {
+    return ( <h1>ExpenseList Here</h1> );
+}
+ 
+export default ExpenseList;
