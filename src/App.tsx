aff8ecfdaec7e7aaf3e48262552ea58d1ddbf3cb
Antonis Gorakis <agorakis@gmail.com>
2023-04-21 00:21:32 +0300

    Finalize Expense List

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,63 @@
+import { useState } from "react";
 import { BsCalculator, BsFillCreditCardFill } from "react-icons/bs";
 import ExpenseList from "./ExpenseList";
 
 function App() {
+  const [expenseList, setExpenseList] = useState([
+    {
+      id: 1,
+      description: "Petrol",
+      amount: 50,
+      category: "Vehicle Expenses",
+    },
+    {
+      id: 2,
+      description: "Meat,Vegetables,Milk",
+      amount: 27,
+      category: "Super Market",
+    },
+    {
+      id: 3,
+      description: "Electricity Bill",
+      amount: 71,
+      category: "Utilities",
+    },
+    {
+      id: 4,
+      description: "Appt Rent for January",
+      amount: 750,
+      category: "Rent",
+    },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenseList(expenseList.filter((item) => item.id !== id));
+  };
+
   return (
     <>
       <div className="p-4 bg-light text-center">
-        <h2 className="pb-4"> <BsCalculator color="green" size={30}/> Manage your expenses <BsFillCreditCardFill color="orange" size={30}/> efficiently </h2>
+        <h2 className="pb-5">
+          <BsCalculator color="green" size={40} />
+          Manage your expenses <BsFillCreditCardFill
+            color="orange"
+            size={30}
+          />{" "}
+          efficiently
+        </h2>
 
         <div className="row row-cols-1 row-cols-lg-2 justify-content-around gy-3">
           <div className="col">One of two columns</div>
-          <div className="col"><ExpenseList/></div>
+          <div className="col">
+            <ExpenseList data={expenseList} onDelete={handleDelete} />
+          </div>
         </div>
       </div>
-      <div className="ps-2 bg-secondary text-warning"> Coded by <span className="fw-bold fst-italic">antGor...</span></div>
+      <div className="ps-2 bg-secondary text-warning">
+        Designed by <span className="fw-bold fst-italic">@antGor</span>
+      </div>
     </>
   );
 }
 
 export default App;
-
-
--- src/ExpenseList.tsx
+++ src/ExpenseList.tsx
@@ -1,5 +1,62 @@
-const ExpenseList = () => {
-    return ( <h1>ExpenseList Here</h1> );
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
 }
- 
+
+interface ExpenseListProps {
+  data: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ data, onDelete }: ExpenseListProps) => {
+  const totalAmount = (data: Expense[]) => {
+    let total = 0;
+
+    data.forEach((item: Expense) => {
+      total = total + item.amount;
+    });
+
+    return total;
+  };
+
+  return (
+    <table className="table ">
+      <thead>
+        <tr>
+          <th scope="col">Description</th>
+          <th scope="col">Amount</th>
+          <th scope="col">Category</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.description}</td>
+            <td>{item.amount}€</td>
+            <td>{item.category}</td>
+            <td>
+              <button
+                className="btn btn-danger"
+                onClick={() => onDelete(item.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+
+        <tr>
+          <td className="fw-bold">Total</td>
+          <td className="fw-bold">{totalAmount(data)}€</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
 export default ExpenseList;
