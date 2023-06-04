0634bc22373f45b1a97da9800b309deee31acc9b
Antonis Gorakis <agorakis@gmail.com>
2023-06-04 08:39:06 +0300

    Add Responsive Table

--- src/ExpenseList.tsx
+++ src/ExpenseList.tsx
@@ -22,44 +22,46 @@ const ExpenseList = ({ data, onDelete }: ExpenseListProps) => {
   };
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Description</th>
-          <th scope="col">Amount</th>
-          <th scope="col">Category</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.length > 0 ? (
-          data.map((item) => (
-            <tr key={item.id}>
-              <td>{item.description}</td>
-              <td>{item.amount}€</td>
-              <td>{item.category}</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => onDelete(item.id)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))
-        ) : (
-          <p className="my-5">There are no expenses in the list yet.</p>
-        )}
+    <div className="table-responsive">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Description</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Category</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 ? (
+            data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.description}</td>
+                <td>{item.amount}€</td>
+                <td>{item.category}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => onDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <p className="my-5">There are no expenses in the list yet.</p>
+          )}
 
-        <tr className="table-primary">
-          <td className="fw-bold">Total</td>
-          <td className="fw-bold">{totalAmount(data)}€</td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+          <tr className="table-primary">
+            <td className="fw-bold">Total</td>
+            <td className="fw-bold">{totalAmount(data)}€</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
